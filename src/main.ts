51da90750e8badcb2f829816e01905955f2c65c6
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-05-04 13:49:24 +0300

    create server chat

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  app.enableShutdownHooks();
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
