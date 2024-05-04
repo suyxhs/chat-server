9f2bffd2c018cc6e90775625d71bfd10b25003b0
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-05-04 13:58:17 +0300

    add new url in index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 const app = () => {
-  const socket = io('http://localhost:3000');
+  const socket = io('https://chat-server-x5i5.onrender.com');
   const msgInput = document.querySelector('.message-input');
   const msgList = document.querySelector('.messages-list');
   const sendBtn = document.querySelector('.send-btn');
@@ -8,7 +8,7 @@ const app = () => {
 
   const getMessages = async () => {
     try {
-      const { data } = await axios.get('http://localhost:3000/api/chat');
+      const { data } = await axios.get('https://chat-server-x5i5.onrender.com/api/chat');
 
       renderMessages(data);
 
